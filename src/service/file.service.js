c219c95e8c8fbcd8b052febaa5e176cee3f188a5
ksjsczy <1361543489@qq.com>
2022-05-09 17:28:23 +0800

    修改1

--- src/service/file.service.js
+++ src/service/file.service.js
@@ -19,6 +19,7 @@ class FileService {
     async getFileByFileName(fileName) {
         const statement = `SELECT * FROM file WHERE filename = ?;`
         const [result] = await connections.execute(statement, [fileName])
+        console.log('这里做出了修改');
         return result[0]
     }
 }
